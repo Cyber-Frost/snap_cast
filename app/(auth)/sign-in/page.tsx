12dd7bda9cb5f7f2bcdf76b039f467f10ff34005
Cyber-Frost <moroz_n_wat2006@yahoo.com>
2025-05-11 21:37:55 +0300

    Implement Google authentication

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { authClient } from "@/lib/auth.client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const SignInPage = () => {
+  const handleSignIn = async () => {
+    return await authClient.signIn.social({ provider: "google" });
+  };
+
+  return (
+    <main className={"sign-in"}>
+      <aside className={"testimonial"}>
+        <Link href={"/"}>
+          <Image
+            src={"/assets/icons/logo.svg"}
+            alt={"logo"}
+            width={32}
+            height={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+        <div className={"description"}>
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, i) => (
+                <Image
+                  src={"/assets/icons/star.svg"}
+                  alt={"star"}
+                  width={20}
+                  height={20}
+                  key={i}
+                />
+              ))}
+            </figure>
+            <p>
+              SnapCast makes screen recording easy. From quick walkthroughes to
+              full presentations, it&#39;s fast, smooth, and shareable in
+              seconds
+            </p>
+            <article>
+              <Image
+                src={"/assets/images/jason.png"}
+                alt={"jason"}
+                width={64}
+                height={64}
+                className={"rounded-full"}
+              />
+              <div>
+                <h2>Vasyl Petrovych</h2>
+                <p>Product Designer, NovaByte</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p>&copy; SnapCast {new Date().getFullYear()}</p>
+      </aside>
+      <aside className={"google-sign-in"}>
+        <section>
+          <Link href={"/"}>
+            <Image
+              src={"/assets/icons/logo.svg"}
+              alt={"logo"}
+              width={40}
+              height={40}
+            />
+            <h1>SnapCast</h1>
+          </Link>
+          <p>
+            Create and share your very first <span>SnapCast video</span> in no
+            time!
+          </p>
+          <button onClick={handleSignIn}>
+            <Image
+              src={"/assets/icons/google.svg"}
+              alt={"google"}
+              width={22}
+              height={22}
+            />
+            <span>Sign in with Google</span>
+          </button>
+        </section>
+      </aside>
+      <div className={"overlay"} />
+    </main>
+  );
+};
+
+export default SignInPage;
---
+++ xata.ts
@@ -0,0 +1,28 @@
+import type { BaseClientOptions } from "@xata.io/client";
+import { buildClient } from "@xata.io/client";
+
+export type DatabaseSchema = {};
+
+const DatabaseClient = buildClient();
+
+const defaultOptions = {
+  databaseURL:
+    "https://Cyber-Frost-s-workspace-lft66s.eu-central-1.xata.sh/db/snap-cast",
+  apiKey: process.env.XATA_API_KEY,
+  branch: "main",
+};
+
+export class XataClient extends DatabaseClient<DatabaseSchema> {
+  constructor(options?: BaseClientOptions) {
+    super({ ...defaultOptions, ...options });
+  }
+}
+
+let instance: XataClient | undefined = undefined;
+
+export const getXataClient = () => {
+  if (instance) return instance;
+
+  instance = new XataClient();
+  return instance;
+};
